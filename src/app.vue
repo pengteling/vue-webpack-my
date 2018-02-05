44ab41689436b111f7cae32ed391045d6c4f9e18
pengteling <pengteling@gmail.com>
2018-02-05 10:14:39 +0800

    333

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="app">   
     <Video></Video>
+    <!-- <mplayer :opt="opt" @loadedmetadata="loadedmetadata"></mplayer> -->
   </div>
 </template>
 <script>
 
 import Video from './Video.vue'
-
+//import mplayer from 'vue-simple-player'
 export default {
   components: {    
-    Video
+    Video,
+    // mplayer
+  },
+  data(){
+    return {
+      opt:{
+        url:'http://oj4t8z2d5.bkt.clouddn.com/sound-of-silence.mp3',
+        autoplay:false,
+        id:"m2"
+      }
+    }
+  },
+  methods:{
+    loadedmetadata(time,id){
+      console.log(time,id)
+
+    }
   }
 };
 </script>
